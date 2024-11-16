9694631d0f26682a477ea2078123473768c79dc8
Adan Aamir <adanaamir2005@gmail.com>
2024-11-16 14:51:43 +0500

    .

--- main.py
+++ main.py
@@ -1,66 +1,45 @@
-#The reason you're using dotenv here is to securely manage your credentials by storing them in a .env file,
-# rather than hardcoding them directly in your script.
+from requests.auth import HTTPBasicAuth
+from requests_oauthlib import OAuth2Session
 from dotenv import load_dotenv   
-import os   #os is often used for working with environment variables (here im retrieving the env variables that are loaded from your .env file) 
-import base64
-from requests import post, get
-import json
+import os , requests
+
 
 load_dotenv()
 
+#STEP1: GET ENV VARIABLES 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+redirect_uri = "https://oauth.pstmn.io/v1/browser-callback"  #this can be any url
 
-def get_token():
-    auth_string = client_id + ":" + client_secret  #now that i have this string, i need to encode it (done in next step)
-    auth_bytes = auth_string.encode("utf-8")   #encoding this using "utf-8", then im gonna encode this using base64 (done in next step)
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")     #converting the returned base64 object into a string so that we can pass it with headers when we send requests to the say account center api
+authorization_base_url = "https://accounts.spotify.com/authorize"   #this and token_url are required necessary for OAuth
+token_url = "https://accounts.spotify.com/api/token"
 
-    url = "https://accounts.spotify.com/api/token"   #url to which i wanna send request to
+scope = ["user-top-read"]    #Permissions your app requests from the user
 
-    headers = {   #headers are gonna be associated with our requests, we're gonna send a post request to the upper url --- These are required by spotify
-        "Authorization": "Basic " + auth_base64,  #here we are sending our autho data, where we will verify that everything is correct and a token will be sent to us
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}  #as it said on the website, i need to pass in a grant type, which here is client credentials 
-    
-    #now we are ready to formulate a response, it will send an HTTP post request to spotifys api
-    result = post(url, headers=headers, data=data)  #here we will get a json data, we will convert it to a python dictionary(next step)
-    json_result = json.loads(result.content)   #loads mean load from string
+#STEP:2 Creating a session using OAuth to manage authetication process
+spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
 
-    #parsing  my token, it will be stored in field called access token
-    token = json_result["access_token"]
-    return token
+#STEP:3 This step generates the URL to redirect the user to Spotify for login and permission approval.
+authorization_url, state = spotify.authorization_url(authorization_base_url)
+print("Please go here and authorize: ", authorization_url)
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}   #autho header for any future requests
+#STEP:4 After the user logs in, Spotify redirects them to your redirect_url with an authorization code as a query parameter. The user then pastes the URL in exchange for an access token
+redirect_response = input("\n\nPaste the full redirect URL here: ")
 
-#function that allows us to search for an artist
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)   #getting the header (function) i made earlier
-    query = f"q={artist_name}&type=artist&limit=1"  #if i was looking for an artist as well as track, then then type would be artist,track -- limit=1 means that when i search for an artist, the post popular one pops out
+#STEP:5 Fetching the access token : in exchange with autho code from provided URL
+auth = HTTPBasicAuth(client_id, client_secret)
+token_info = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)  #fetchtokek typically return the access toke inside the dic so thats why ie done the below step
+token = token_info['access_token']   #extracting the access token
 
-    query_url = url+ "?" + query
-    result = get(query_url, headers=headers) 
-    json_result = json.loads(result.content)["artists"]["items"]
-    
-    if len(json_result) == 0:
-        print("No artist with this name exists")
-        return None
-    return json_result[0]  #else return the very first result
+top_tracks_url = "https://api.spotify.com/v1/me/top/tracks"
+headers = {
+    "Authorization": f"Bearer {token}" 
+}
+response = requests.get(top_tracks_url, headers=headers)
 
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"   #here i have my base url, and iam looking for a specific artist 
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
+#STEP:6 Using the access token
+# r = spotify.get("https://api.spotify.com/v1/me")  #contains info about the user
+top_tracks = response.json()
 
-token = get_token()
-result = search_for_artist(token, "Chase Atlantic")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-for idx, song in enumerate(songs):   #printing top 10 songs by the artist
-    print(f"{idx+1}. {song['name']}")  #prints the songs rank and name
+for idx, tracks in enumerate(top_tracks['items']):
+    print(f"{idx+1}. Track Name: {tracks['name']}, Artist: {tracks['artists'][0]['name']}")
