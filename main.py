ec4d95dd74008099ef0007977c6d65241b5b0ded
Adan Aamir <adanaamir2005@gmail.com>
2024-11-21 21:00:47 +0500

    testting with colours

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 from dotenv import load_dotenv
-import os, spotipy, requests
+import os, spotipy, requests, time
 from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth 
 from spotipy.oauth2 import SpotifyOAuth
 
 class UserOptions:
     def __init__(self):
+        self.op = None
+
+    def get_option(self):
         print("1. Get playlists recommended according to the mood entered\n2. Log-in to your spotify to get your top tracks displayed")
         while True:
-            try:          
+            try:
                 self.op = int(input("Please enter any option(1/2): "))
                 if self.op in [1,2]:
                     break
                 else:
-                    print("Not an option. Enter a correct option")
+                    print("Not an option. Enter a correct option(1/2)")
             except ValueError:   
                 print("Incorrect type: \"str\" entered. Enter an int.")
+        return self.op
 
 class Credentials:
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret    
-        self.redirect_url = "https://oauth.pstmn.io/v1/browser-callback"
-        self.authorization_base_url = "https://accounts.spotify.com/authorize"
-        self.scope = ["user-top-read"]
+        redirect_url = "https://oauth.pstmn.io/v1/browser-callback"
+        authorization_base_url = "https://accounts.spotify.com/authorize"
+        scope = ["user-top-read"]
 
-    def authorization(self):
-        self.spotify = OAuth2Session(self.client_id, scope=self.scope, redirect_uri=self.redirect_url)
-        authorization_url, _ = self.spotify.authorization_url(self.authorization_base_url, prompt='login')
+        self.spotify = OAuth2Session(self.client_id, scope=scope, redirect_uri=redirect_url)
+        authorization_url, _ = self.spotify.authorization_url(authorization_base_url, prompt='login')
         print(f"\nVisit here and login: {authorization_url}")
 
         while True: 
@@ -37,7 +40,7 @@ class Credentials:
                     break
             except ValueError:
                 print("Invalid URL format. Ensure the url starts with \'https://\'")
-
+ 
     def fetchingAccessToken(self):
         token_url = "https://accounts.spotify.com/api/token"
         top_tracks_url = "https://api.spotify.com/v1/me/top/tracks"
@@ -57,9 +60,13 @@ class Credentials:
         
         return response
 
-class PlaylistRecommendation(Credentials):
+class PlaylistRecommendation:
     def __init__(self, client_id, client_secret):
-        super().__init__(client_id, client_secret)
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.green = "\033[34m"
+        self.reset = "\033[0m"
+        self.yellow = "\033[33m"
 
     def enterMood(self):
         self.moods = {
@@ -67,7 +74,7 @@ class PlaylistRecommendation(Credentials):
             "2" : "Sad",
             "3" : "Chill",
             "4" : "Peaceful", 
-            "5" : "Energetic/Upbeat",
+            "5" : "Energetic",
             "6" : "Focused",
             "7" : "Angry",
             "8" : "Motivational" 
@@ -85,20 +92,19 @@ class PlaylistRecommendation(Credentials):
                 print("Enter a valid option")
 
     def authorization(self):
-        super().authorization()
         if os.path.exists(".cache"):
             os.remove(".cache")
 
         redirect_url = "https://oauth.pstmn.io/v1/browser-callback"
-        scope = "playlist-read-private" 
+        scope = "playlist-read-private"
 
         mood_to_genre = {
             "Happy": "pop",
             "Sad": "acoustic",
             "Chill": "chill",
             "Peaceful": "ambient",
-            "Energetic/Upbeat": "dance",
-            "Focused": "focus",
+            "Energetic": "dance",
+            "Focused": "study",
             "Angry": "metal",
             "Motivational": "work-out"
         }
@@ -108,33 +114,64 @@ class PlaylistRecommendation(Credentials):
         print("\nFetching recommended playlists...\n")
         
         results = sp.recommendations(seed_genres=[genre],limit=10)  #fetching recommendations using the built in funtion
-
+        
         for idx, track in enumerate(results['tracks']):
-            print(f"{idx+1} Track: {track['name']} by {track['artists'][0]['name']}, URL: {track['external_urls']['spotify']}")
+            print(f"{idx+1} Track: {self.yellow}{track['name']}{self.reset} by {track['artists'][0]['name']}, URL: {self.green}{track['external_urls']['spotify']}{self.reset}")
 
-class TopTracks(Credentials):
-    def __init__(self, client_id, client_secret,response):
-        super().__init__(client_id, client_secret)
+        option = input("Type:\n\"C\" to get more recomendations \n\"E\" to exit: ").upper()
+        if option == "C":
+            results = sp.recommendations(seed_genres=[genre],limit=10)
+            for idx, track in enumerate(results['tracks']):
+                print(f"{idx+1} Track: {self.yellow}{track['name']}{self.reset} by {track['artists'][0]['name']}, URL: {self.green}{track['external_urls']['spotify']}{self.reset}")
+            
+        elif option == "E":
+            print("\nExiting the program", end="", flush=True)
+            for _ in range(3):
+                time.sleep(0.5)
+                print(".", end="", flush=True)
+            print()
+            exit()
+
+class TopTracks:
+    def __init__(self,response):
         self.response = response
+        self.green = "\033[34m"
+        self.reset = "\033[0m"
+        self.yellow = "\033[33m"
 
-        if self.response.status_code != 200:
-            print(f"\nError: {self.response.status_code} - {self.response.text}")
+        if response.status_code != 200:
+            print(f"\n{self.red}Error: {response.status_code} - {response.text}{self.reset}")
             exit()
         try:
             self.top_tracks = self.response.json()
         except requests.exceptions.JSONDecodeError:
-            print(f"\nError: Failed to decode the JSON response - {self.response.text}")
+            print(f"\n{self.red}Error: Failed to decode the JSON response - {response.text}{self.reset}")
             exit()
-    
+
     def displayTopTracks(self):
-        print("\nFetching user's top tracks...\n")
+        print("\nFetching user's top tracks", end="", flush=True)
+        for _ in range(3):
+            time.sleep(0.5)
+            print(".", end="" ,flush=True)
+        print()
 
         if not self.top_tracks.get('items', []):
             print("No data was found")
             exit()
 
         for idx, track in enumerate(self.top_tracks['items']):
-            print(f"{idx+1}. Track Name: {track['name']} Artist Name: {track['artists'][0]['name']}")
+            print(f"{idx+1}. {self.yellow}Track Name: {track['name']}{self.reset} Artist Name: {track['artists'][0]['name']}")
+
+        option = input("Type:\n\"C\" to get more recomendations \n\"E\" to exit: ").upper()
+        if option == "C":
+            ...
+        elif option == "E":
+            print("\nExiting the program", end="", flush=True)
+            for _ in range(3):
+                time.sleep(0.5)
+                print(".", end="", flush=True)
+            print()
+            exit()
 
 if __name__ == "__main__":
     load_dotenv()
@@ -146,17 +183,15 @@ if __name__ == "__main__":
         exit()
     
     choice = UserOptions()
+    op = choice.get_option()
 
-    if choice.op == 1:
-        credentials = Credentials(client_id, client_secret)
-        credentials.authorization()
+    if op == 1:
         recommend = PlaylistRecommendation(client_id, client_secret)
         recommend.enterMood()
         recommend.authorization()
 
-    elif choice.op == 2:
+    elif op == 2:
         credentials = Credentials(client_id, client_secret)
-        credentials.authorization()
         response = credentials.fetchingAccessToken()
-        top_tracks = TopTracks(client_id, client_secret, response)
+        top_tracks = TopTracks(response)
         top_tracks.displayTopTracks()
